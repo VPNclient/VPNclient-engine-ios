a37ef37e86482c43154fbf27e1555218ebee7765
Anton Dodonov <anton.v.dodonov@gmail.com>
2025-03-27 20:40:13 +0700

    pods

---
+++ VPNclientEngineIOS/VPNclientEngineIOS.h
@@ -0,0 +1,18 @@
+//
+//  VPNclientEngineIOS.h
+//  VPNclientEngineIOS
+//
+//  Created by Anton Dodonov on 27/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VPNclientEngineIOS.
+FOUNDATION_EXPORT double VPNclientEngineIOSVersionNumber;
+
+//! Project version string for VPNclientEngineIOS.
+FOUNDATION_EXPORT const unsigned char VPNclientEngineIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPNclientEngineIOS/PublicHeader.h>
+
+
